556426308e41a7bca909a8fb053ed3949713a431
Manish Arora <manish.arora1392@gmail.com>
2022-06-08 23:43:35 +0530

    Added Unit Test Project

---
+++ SearchAppUnitTests/BLL/ImageBusinessLayerUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SearchPhotosApp.Models;
+using SearchPhotosApp.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchPhotosApp.Services;
+using Moq;
+
+namespace SearchPhotosAppUnitTests.BLL
+{
+    public class ImageBusinessLayerUnitTests
+    {
+
+        private readonly Mock<IImageService> _service;
+        public ImageBusinessLayerUnitTests()
+        {
+            _service = new Mock<IImageService>();
+        }
+        [Test]
+        public void GetFeed_Success()
+        {
+            FeedModel feed = new FeedModel();
+            _service.Setup(x => x.GetFeed(It.IsAny<string>())).Returns(Task.FromResult(It.IsAny<string>()));
+            ImageBusinessLayer imgBLL = new ImageBusinessLayer(_service.Object);
+            var result = imgBLL.GetFeed(It.IsAny<string>());
+            Assert.IsNotNull(result);
+        }
+    }
+}
